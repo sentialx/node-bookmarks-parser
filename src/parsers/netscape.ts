cd248b431a5089b319f4b09a1a9d6203c53dd18d
Sential <sentialx@gmail.com>
2019-07-29 22:06:12 +0200

    refactor: rewrite to cheerio

--- src/parsers/netscape.ts
+++ src/parsers/netscape.ts
@@ -1,4 +1,4 @@
-import { JSDOM } from 'jsdom';
+import { load } from 'cheerio';
 import { Bookmark } from '../interfaces/bookmark';
 
 export const canParse = (html: string) => {
@@ -21,30 +21,32 @@ export const canParse = (html: string) => {
   );
 };
 
-const getNodeData = (node: any) => {
+const getNodeData = ($: CheerioStatic, node: CheerioElement) => {
   const data: Bookmark = {};
 
   for (let i = 0; i < node.childNodes.length; i++) {
-    if (node.childNodes[i].tagName === 'A') {
-      data.type = 'bookmark';
-      data.url = node.childNodes[i].getAttribute('href');
-      data.title = node.childNodes[i].textContent;
+    const child = $(node.childNodes[i]);
 
-      const addDate = node.childNodes[i].getAttribute('add_date');
+    if (node.childNodes[i].tagName === 'a') {
+      data.type = 'bookmark';
+      data.url = child.attr('href');
+      data.title = child.text();
+
+      const addDate = child.attr('add_date');
       if (addDate) {
         data.addDate = addDate;
       }
 
-      const icon = node.childNodes[i].getAttribute('icon');
+      const icon = child.attr('icon');
       if (icon) {
         data.icon = icon;
       }
-    } else if (node.childNodes[i].tagName === 'H3') {
+    } else if (node.childNodes[i].tagName === 'h3') {
       data.type = 'folder';
-      data.title = node.childNodes[i].textContent;
+      data.title = child.text();
 
-      const addDate = node.childNodes[i].getAttribute('add_date');
-      const lastModified = node.childNodes[i].getAttribute('last_modified');
+      const addDate = child.attr('add_date');
+      const lastModified = child.attr('last_modified');
 
       if (addDate) {
         data.addDate = addDate;
@@ -54,20 +56,20 @@ const getNodeData = (node: any) => {
         data.lastModified = lastModified;
       }
       data.nsRoot = null;
-      if (node.childNodes[i].hasAttribute('personal_toolbar_folder')) {
+      if (child.attr('personal_toolbar_folder')) {
         data.nsRoot = 'toolbar';
       }
-      if (node.childNodes[i].hasAttribute('unfiled_bookmarks_folder')) {
+      if (child.attr('unfiled_bookmarks_folder')) {
         data.nsRoot = 'unsorted';
       }
-    } else if (node.childNodes[i].tagName === 'DL') {
+    } else if (node.childNodes[i].tagName === 'dl') {
       (data as any).__dir_dl = node.childNodes[i];
     }
   }
 
   if (data.type === 'folder' && !(data as any).__dir_dl) {
     if (node.nextSibling && node.nextSibling.tagName === 'DD') {
-      const dls = node.nextSibling.getElementsByTagName('DL');
+      const dls = $(node.nextSibling).find('DL');
       if (dls.length) {
         (data as any).__dir_dl = dls[0];
       }
@@ -77,7 +79,7 @@ const getNodeData = (node: any) => {
   return data;
 };
 
-const processDir = (dir: any, level: number) => {
+const processDir = ($: CheerioStatic, dir: any, level: number) => {
   const children = dir.childNodes;
   let menuRoot: Bookmark = null;
 
@@ -88,10 +90,10 @@ const processDir = (dir: any, level: number) => {
     if (!child.tagName) {
       continue;
     }
-    if (child.tagName !== 'DT') {
+    if (child.tagName !== 'dt') {
       continue;
     }
-    const itemData = getNodeData(child);
+    const itemData = getNodeData($, child);
 
     if (itemData.type) {
       if (level === 0 && !itemData.nsRoot) {
@@ -103,13 +105,21 @@ const processDir = (dir: any, level: number) => {
           };
         }
         if (itemData.type === 'folder' && (itemData as any).__dir_dl) {
-          itemData.children = processDir((itemData as any).__dir_dl, level + 1);
+          itemData.children = processDir(
+            $,
+            (itemData as any).__dir_dl,
+            level + 1,
+          );
           delete (itemData as any).__dir_dl;
         }
         menuRoot.children.push(itemData);
       } else {
         if (itemData.type === 'folder' && (itemData as any).__dir_dl) {
-          itemData.children = processDir((itemData as any).__dir_dl, level + 1);
+          itemData.children = processDir(
+            $,
+            (itemData as any).__dir_dl,
+            level + 1,
+          );
           delete (itemData as any).__dir_dl;
         }
         items.push(itemData);
@@ -123,12 +133,11 @@ const processDir = (dir: any, level: number) => {
 };
 
 export const parse = (html: string) => {
-  const { window } = new JSDOM(html);
-
-  const dls = window.document.getElementsByTagName('DL');
+  const $ = load(html);
+  const dls = $('dl');
 
   if (dls.length > 0) {
-    return processDir(dls[0], 0);
+    return processDir($, dls[0], 0);
   }
 
   throw new Error(
